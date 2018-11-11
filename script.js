120c3bba4e9b5edefac5beb720dd230b2a5d107a
Imper1uss <imper1uss666@gmail.com>
2018-11-11 21:07:27 +0200

    kr1

---
+++ script.js
@@ -0,0 +1,55 @@
+var blockModal = document.getElementById("modal");
+console.log(blockModal);
+var btnLink = document.getElementById("btn-link1");
+console.log(btnLink);
+btnLink.addEventListener('click', function () {
+    blockModal.style.display = "block";
+})
+
+var btnLink2 = document.getElementById("btn-link2");
+console.log(btnLink2);
+btnLink2.addEventListener('click', function () {
+    blockModal.style.display = "block";
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  
+  
+  function setRandomColor() {
+    $("#colorpad").css("background-color", getRandomColor());
+  }
